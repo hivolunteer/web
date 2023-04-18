49a784f122e29753090c8d2b5d00bb0252ad9f81
Louis-Henri MC ALLISTER <louis-henri.mc-allister@epitech.eu>
2023-04-18 17:50:51 +0200

    HIVE-26 [ADD] basic template of react app in typescript & update .gitignore with all ignoring options

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// import Pages
-import Login from './Login/Login';
-import Register from './Register/Register';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
