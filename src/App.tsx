515f88cb0cbf464c42f2010cb55762dba43d18b8
Petro Floresku <petro.floresku@epitech.eu>
2023-05-01 11:58:56 +0200

    HIVE-29: [UPD] Mise en place de du rooting

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import React from "react";
+import React from 'react';
 import './App.scss';
-import titleLogo from './images/titleLogo.png';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserTypeChoice from "./pages/userTypeChoice/userTypeChoice";
 
-const App = () => {
-
-    return(
-     <div className="container">
-         <div className="row">
-             <h1 className="title">
-                 Qui êtes vous ?
-             </h1>
-         </div>
-         <div className="row">
-             <div className="col-12">
-                 <button className="btn entreprise btn-lg">
-                     entreprise
-                 </button>
-             </div>
-         </div>
-         <div className="row">
-             <div className="col-6">
-                 <img className="img-fluid titleLogo" src={titleLogo} alt=""/>
-             </div>
-             <div className="col-6">
-                 <button className="btn association btn-lg" style={{backgroundColor: "darkolivegreen"}}>
-                     association
-                 </button>
-             </div>
-         </div>
-         <div className="row">
-             <div className="col-12">
-                 <button className="btn benevole btn-lg" style={{backgroundColor: "yellowgreen"}}>
-                     bénévole
-                 </button>
-             </div>
-         </div>
-     </div>
-    );
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserTypeChoice />} />
+        <Route path="/register" element={<UserTypeChoice />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/userTypeChoice/userTypeChoice.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './App.scss';
+import titleLogo from '../../images/titleLogo.png';
+
+const UserTypeChoice = () => {
+
+    return(
+        <div className="container">
+            <div className="row">
+                <h1 className="title">
+                    Qui êtes vous ?
+                </h1>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <button className="btn entreprise btn-lg">
+                        entreprise
+                    </button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    <img className="img-fluid titleLogo" src={titleLogo} alt=""/>
+                </div>
+                <div className="col-6">
+                    <button className="btn association btn-lg" style={{backgroundColor: "darkolivegreen"}}>
+                        association
+                    </button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <button className="btn benevole btn-lg" style={{backgroundColor: "yellowgreen"}}>
+                        bénévole
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserTypeChoice;
