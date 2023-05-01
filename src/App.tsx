01d7dfa2f10dfe352dc5d08bfb5e0c48bfec0c12
Petro Floresku <petro.floresku@epitech.eu>
2023-05-01 11:50:53 +0200

    HIVE-29: [ADD] créer page de choix (entreprise/association/bénévole)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserTypeChoice from "./pages/userTypeChoice/userTypeChoice";
-import Sidebar from './sidebar/Sidebar';
+import Sidebar from "./sidebar/Sidebar";
 
 // import Pages
-import Profile from './pages/Profile/Profile';
-import Home from './pages/Home/Home';
-import RegisterVolunteer from './pages/Authentication/RegisterVolunteer/Register';
-import RegisterAssociation from './pages/Authentication/RegisterAssociation/Register';
-import LoginVolunteer from './pages/Authentication/LoginVolunteer/Login';
-import LoginAssociation from './pages/Authentication/LoginAssociation/Login';
+import Profile from "./pages/Profile/Profile";
+import Home from "./pages/Home/Home";
+import RegisterVolunteer from "./pages/Authentication/RegisterVolunteer/Register";
+import RegisterAssociation from "./pages/Authentication/RegisterAssociation/Register";
+import LoginVolunteer from "./pages/Authentication/LoginVolunteer/Login";
+import LoginAssociation from "./pages/Authentication/LoginAssociation/Login";
 
 function App() {
   return (
