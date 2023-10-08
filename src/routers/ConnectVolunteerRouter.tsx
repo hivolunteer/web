ee7a14304d886e3f248e1fa283ae7b6584e2fd3f
Thibault CASTILLON <thibault.castillon@epitech.eu>
2023-10-08 17:28:51 +0100

    <Hive-95> [ADD] Historisation des Missions + Mission Card

--- src/routers/ConnectVolunteerRouter.tsx
+++ src/routers/ConnectVolunteerRouter.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Profile from '../pages/Volunteer/Profile/Profile';
+import Home from '../pages/Volunteer/Home/Home';
 
 function ConnectVolunteerRouter() {
     return(
         <Routes>
+            <Route path='/' element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<h1> Error 404 Volunteer </h1>} />
         </Routes>
