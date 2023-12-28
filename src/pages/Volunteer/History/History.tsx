e38fef2d74bef74b8b91c118160985171143f4b3
Thibault CASTILLON <thibault.castillon@epitech.eu>
2023-12-28 18:26:17 +0100

    [ADD] History Page for Volunteer - Allows to see old missions

---
+++ src/pages/Volunteer/History/History.tsx
@@ -0,0 +1,56 @@
+import {useState, useEffect} from "react";
+import config from "../../../config";
+import RateCard from "./RateCard";
+
+
+function History() {
+
+    const [missions, setMissions] = useState<number[]>([])
+        
+    useEffect(() => {
+        fetch(`${config.apiUrl}/missions/volunteer/passed`, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }).then((response) => {
+            if (response.status === 200) {
+                response.json().then((data) => {
+                    setMissions(data.passed_missions)
+                })
+            }
+        })
+    }, [])
+    
+    return (
+        <div style={{width: '90%', margin: 'auto 5%'}}>
+            <div className="History-Header">
+                <h1> Vos Missions Passées </h1>
+            </div>
+            <div className="History-Body">
+                {
+                    (missions.length > 0) ? (
+                        <div>
+                            {
+                                missions.map((mission) => {
+                                    return (
+                                        <div>
+                                            <RateCard mission={mission} />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    ) : (
+                        <div>
+                            <p> Vous n'avez pas encore participé à de missions. </p>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default History
--- src/routers/ConnectVolunteerRouter.tsx
+++ src/routers/ConnectVolunteerRouter.tsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Profile from '../pages/Volunteer/Profile/Profile';
-import Home from '../pages/Volunteer/Home/Home';
-import FollowAssociation from '../pages/Volunteer/FollowAssociation/FollowAssociation';
+import { Routes, Route } from "react-router-dom";
+import Profile from "../pages/Volunteer/Profile/Profile";
+import Home from "../pages/Volunteer/Home/Home";
+import FollowAssociation from "../pages/Volunteer/FollowAssociation/FollowAssociation";
+import History from "../pages/Volunteer/History/History";
 
 function ConnectVolunteerRouter() {
-    return(
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/follows' element={<FollowAssociation />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="*" element={<h1> Error 404 Volunteer </h1>} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/follows" element={<FollowAssociation />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<h1> Error 404 Volunteer </h1>} />
+    </Routes>
+  );
 }
 
 export default ConnectVolunteerRouter;
