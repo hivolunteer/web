9512b74f4b4f6fe3c85a42f52e0b609e5b36029a
Thibault CASTILLON <thibault.castillon@epitech.eu>
2023-12-28 23:43:50 +0100

    [ADD] Association Follow

---
+++ src/pages/Volunteer/FollowAssociation/FollowAssociation.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import config from "../../../config";
+import './FollowAssociation.scss';
+import AssociationCard from "./AssociationCard";
+
+export default function FollowAssociation() {
+    
+    const [associations, setAssociations] = useState<number[]>([]);
+
+    useEffect(() => {
+        fetch(`${config.apiUrl}/follows/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }).then((response: any) => {
+            if (response.status === 200) {
+                response.json().then((data: any) => {
+                    console.log(data);
+                    setAssociations(data);
+                })
+            }
+        })
+    }, [])
+
+    return (
+        <div className="follow-association-container">
+            <h1>Associations Suivies</h1>
+            <div>
+                {
+                    associations.length === 0 ? <h2> Vous ne suivez aucune association </h2> :
+                    <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+                    {
+                        associations.map((association: any) => {
+                            return(
+                                <div style={{width: '100%', height: '100%'}}>
+                                    <AssociationCard id={association.association_id} />
+                                </div>
+                            )
+                        })
+                    }
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
--- src/routers/ConnectVolunteerRouter.tsx
+++ src/routers/ConnectVolunteerRouter.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Profile from '../pages/Volunteer/Profile/Profile';
 import Home from '../pages/Volunteer/Home/Home';
+import FollowAssociation from '../pages/Volunteer/FollowAssociation/FollowAssociation';
 
 function ConnectVolunteerRouter() {
     return(
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/follows' element={<FollowAssociation />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<h1> Error 404 Volunteer </h1>} />
         </Routes>
