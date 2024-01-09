6eeecf7673af2ce783c71beb800398f370f6da7b
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-01-09 01:32:25 +0000

    [HOTFIX]

--- src/pages/Volunteer/VolunteerHome/VolunteerHome.tsx
+++ src/pages/Volunteer/VolunteerHome/VolunteerHome.tsx
@@ -194,9 +194,7 @@ function VolunteerHome(props: any) {
                         (filteredMissions.length === 0 || filteredMissions.some((filteredMission: Mission) => mission.id === filteredMission.id)) &&
                         mission.title.toLowerCase().includes(search.toLowerCase())
                         )
-                        .filter((mission: Mission) =>
-                        (filteredAssociations.length === 0 || filteredAssociations.some((filteredAssociation: Association) => mission.status === 1))
-                        )
+                        .filter((mission: Mission) => mission.status === 1)
                         .map((mission: Mission) => (
                         <div className="mission-card" key={mission.id}>
                             <MissionCard mission_id={mission.id} />
