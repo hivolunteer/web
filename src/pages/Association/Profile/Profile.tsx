2ad9555f719c600fb4b25b69d185994ace7c49c8
Petro Floresku <petro.floresku@epitech.eu>
2024-04-30 12:02:33 +0200

    [WEB-1.5]: modification d'indentation et début de changement de page Profile

--- src/pages/Association/Profile/Profile.tsx
+++ src/pages/Association/Profile/Profile.tsx
@@ -1,5 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import * as React from "react";
+import Card from '@mui/material/Card';
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Container } from "@mui/material";
+import {useEffect, useState} from "react";
 import config from "../../../config";
 import "./Profile.scss";
 
@@ -7,19 +13,23 @@ import profileImage from "../../../images/logo/submark.png";
 import EditPasswordModal from "./EditPasswordModal";
 
 type newProfile = {
-  first_name: string;
-  last_name: string;
+  name: string;
+  description: string;
   email: string;
   phone: string;
   profile_picture: string;
 };
 
-function ProfilePage(props: any) {
-  const [first_name, setFirstName] = useState<string>("");
-  const [last_name, setLastName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [phone, setPhone] = useState<string>("");
-  const [profile_picture, setProfilePicture] = useState<string>(profileImage);
+export default function ProfilePage(props: any) {
+
+    const [name, setName] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [phone, setPhone] = useState<string>("");
+    const [profilePicture, setProfilePicture] = useState<string>("");
+    const image =
+        "https://urgo.fr/wp-content/uploads/2022/03/Logo-Reforestaction.png";
+
 
 
   const [openDialog, setOpenDialog] = useState<boolean>(false);
@@ -27,37 +37,37 @@ function ProfilePage(props: any) {
     setOpenDialog(false);
   };
 
-  useEffect(() => {
-    console.log(localStorage);
-    const getProfile = () => {
-      fetch(`${config.apiUrl}/volunteers/profile`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) {
-            response.json().then((data) => {
-              setFirstName(data.volunteer.first_name);
-              setLastName(data.volunteer.last_name);
-              setEmail(data.volunteer.email);
-              setPhone(data.volunteer.phone);
-              setProfilePicture(data.volunteer.profile_picture);
-            });
-          } else {
-            console.log("Error fetching profile");
-            console.log(response);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
+    useEffect(() => {
+        const getProfile = async () => {
+            await fetch(`${config.apiUrl}/associations/profile`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            })
+                .then((response) => {
+                    if (response.status === 200) {
+                        response.json().then((data) => {
+                          setProfilePicture(data.association.profile_picture);
+                            setName(data.association.name);
+                            setDescription(data.association.description);
+                            setEmail(data.association.email);
+                            setPhone(data.association.phone);
+                        });
+                    } else {
+                        console.log("Error fetching profile");
+                        console.log(response);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        };
 
-    getProfile();
-  }, []);
+        getProfile();
+    }, []);
+
 
   function validateEmail(email: string): boolean {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -110,13 +120,13 @@ function ProfilePage(props: any) {
     //   console.error('Invalid phone number');
     //   return;
     // }
-    console.log(first_name, last_name, email, phone, profile_picture);
+    console.log(name, email, phone, profilePicture);
     let profile: newProfile = {
-      first_name: first_name,
-      last_name: last_name,
+      name: name,
       email: email,
       phone: phone,
-      profile_picture: profile_picture,
+      profile_picture: profilePicture,
+      description: ""
     };
 
     fetch(`${config.apiUrl}/volunteers/update`, {
@@ -134,112 +144,99 @@ function ProfilePage(props: any) {
         console.log(error);
       });
   };
+    return (
+    <>
+      <div style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        alignSelf: "self-start",
+        textAlign: "center",
+        padding: "20px",
+      }}>
+        <img src={image} alt="Logo de profil" className={"container-div"} />
+        <h1>Association {name}</h1>
+      </div><div className="container-div">
 
-  /* Function to add when back is gonna be done */
+          <h2>
+            Bénévoles
+          </h2>
+          <Grid container alignItems="stretch" style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: "space-between",
+          }}>
+            <Grid item style={{
+              display: 'flex',
+              marginBottom: "30px",
+            }}>
+              <Card className={"card-component"}>
+                <h4>
+                  0 bénévoles actuels
+                </h4>
+                <h4>
+                  Vous n'avez actuellement aucun bénévole
+                </h4>
+              </Card>
+              <Card className={"card-component"}>
+                <h4>
+                  0 bénévoles en attente de confirmation
+                </h4>
+                <h4>
+                  Vous n'avez actuellement aucun bénévole en attente de confirmation
+                </h4>
+              </Card>
+            </Grid>
+          </Grid>
+          <h2>
+            Description de l'association
+          </h2>
+          <h4 style={{
+            padding: "20px",
+            textAlign: "center",
+          }}>
+            {description === null || description === undefined || description === "" ? "Aucune description n'est disponible pour cette association" : description}
+          </h4>
+          <h2>
+            Contact
+          </h2>
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "flex-start",
+            textAlign: "center",
+            padding: "20px",
+          }}>
+            <h4>
+              {email}
+            </h4>
+            <h4>
+              {phone}
+            </h4>
+          </div>
 
-  /*const deleteAccount = () => {
-     if (window.confirm('Are you sure you want to delete your account?')) {
-      let url = `${config.apiUrl}/volunteers/profile`;
-      fetch(url, {
-        method: 'DELETE',
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
-        },
-      })
-        .then((response) => {
-          if (response.status === 204) {
-            alert('Account deleted successfully');
-            // Redirect to the login page
-            window.location.href = '/';
-          } else {
-            console.log('Error deleting account');
-          }
-        })
-        .catch((error) => 
-          console.log(error);
-        });
-    }
-  };*/
 
-  return (
-    <Container>
-      <Row className="profile-row">
-        <Col sm={12} md={4} lg={3}>
-          <div className="profile-pic">
-            <img src={profile_picture} alt="" className="profile-img" />
-          </div>
-          <div className="profile-btn-div">
-            <label htmlFor="profile-pic-upload" className="profile-pic-btn">
-              Changer la Photo
-            </label>
-            <input
-              className="profile-input"
-              id="profile-pic-upload"
-              type="file"
-              onChange={handleFileChange}
-              accept="image/*"
-            />
-          </div>
-        </Col>
-        <Col sm={12} md={8} lg={9}>
-          <div className="profile-info">
-            <div className="profile-row">
-              <label>Prénom:</label>
-              <input
-                className="filled-text"
-                type="text"
-                placeholder="Name"
-                value={first_name}
-                onChange={(event) => setFirstName(event.target.value)}
-              />
-            </div>
-            <div className="profile-row">
-              <label>Nom de famille:</label>
-              <input
-                className="filled-text"
-                type="text"
-                placeholder="Last Name"
-                value={last_name}
-                onChange={(event) => setLastName(event.target.value)}
-              />
-            </div>
-            <div className="profile-row">
-              <label>Email:</label>
-              <input
-                className="filled-text"
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              />
-            </div>
-            <div className="profile-row">
-              <label>Numéro de téléphone:</label>
-              <input
-                className="filled-text"
-                type="text"
-                placeholder="Phone number"
-                value={phone}
-                onChange={(event) => setPhone(event.target.value)}
-              />
-            </div>
-            <div className="profile-btn-div">
-              <button className="profile-pic-btn" onClick={updateProfile}>
-                Mettre à jour le profile
-              </button>
-              {/* <button className="delete-account-btn" onClick={deleteAccount}>
-                            Supprimer le compte
-                        </button> */}
-              <button className="profile-pic-btn edit" onClick={() => setOpenDialog(true)} style={{backgroundColor: "#FFD700"}}>
-                Modifier le mot de passe
-              </button>
-              <EditPasswordModal modalProps={{ open: openDialog, onClose: closeDialog }} />
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+        </div><div className="profile-row">
+          <label>Numéro de téléphone:</label>
+          <input
+            className="filled-text"
+            type="text"
+            placeholder="Phone number"
+            value={phone}
+            onChange={(event) => setPhone(event.target.value)} />
+        </div><div className="profile-btn-div">
+          <button className="profile-pic-btn" onClick={updateProfile}>
+            Mettre à jour le profile
+          </button>
+          {/* <button className="delete-account-btn" onClick={deleteAccount}>
+                  Supprimer le compte
+              </button> */}
+          <button className="profile-pic-btn edit" onClick={() => setOpenDialog(true)} style={{ backgroundColor: "#FFD700" }}>
+            Modifier le mot de passe
+          </button>
+          <EditPasswordModal modalProps={{ open: openDialog, onClose: closeDialog }} />
+        </div>
+        </>
   );
 }
-
-export default ProfilePage;
