9252434a078494b3022a66f795194903c7c962c6
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-05-04 02:33:28 +0100

    [ADD] Volunteer Search - basic

---
+++ src/pages/Volunteer/Search/Panels/VolunteerPanel.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Volunteer } from "../../../../interfaces";
+import config from "../../../../config";
+import VolunteerCard from "../Cards/VolunteerCard";
+
+interface VolunteerPanelProps {
+    volunteerFilteredList: Array<Volunteer>;
+    search: string;
+}
+
+function VolunteerPanel(props: VolunteerPanelProps) {
+
+    const { volunteerFilteredList, search } = props;
+
+    const [volunteersList, setVolunteersList] = useState<Array<Volunteer>>([]);
+
+    useEffect(() => {
+        fetch(`${config.apiUrl}/volunteers`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setVolunteersList(data);
+            });
+    }, [volunteerFilteredList]);
+
+    return (
+        <div className="missions-container">
+            {
+                volunteersList
+                    .map((volunteer: Volunteer) => (
+                        <div className="mission-card" key={volunteer.id}>
+                            <VolunteerCard volunteer={volunteer} />
+                        </div>
+                    ))
+            }
+        </div>
+    )
+}
+
+export default VolunteerPanel;
