2c8b0c85b074ed2baf6292f2818ab6324a538863
Bill ON <bill.on@epitech.eu>
2024-05-04 21:41:24 +0800

    [FIX] /volunteer/profile : elements are now properly centered

--- src/pages/Volunteer/Profile/Profile.tsx
+++ src/pages/Volunteer/Profile/Profile.tsx
@@ -30,23 +30,23 @@ function ProfilePage(props: any) {
           'Authorization': `Bearer ${localStorage.getItem('token')}`
         },
       })
-      .then((response) => {
-        if (response.status === 200) {
-          response.json().then((data) => {
-            setFirstName(data.volunteer.first_name);
-            setLastName(data.volunteer.last_name);
-            setEmail(data.volunteer.email);
-            setPhone(data.volunteer.phone);
-            setProfilePicture(data.volunteer.profile_picture);
-          });
-        } else {
-          console.log('Error fetching profile');
-          console.log(response)
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+        .then((response) => {
+          if (response.status === 200) {
+            response.json().then((data) => {
+              setFirstName(data.volunteer.first_name);
+              setLastName(data.volunteer.last_name);
+              setEmail(data.volunteer.email);
+              setPhone(data.volunteer.phone);
+              setProfilePicture(data.volunteer.profile_picture);
+            });
+          } else {
+            console.log('Error fetching profile');
+            console.log(response)
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
     }
 
     getProfile();
@@ -57,52 +57,43 @@ function ProfilePage(props: any) {
     return emailRegex.test(email);
   }
 
-  /* function validatePhone(phone: string): boolean {
-    const re = /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/;
-    return re.test(phone);
-  } */
-
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-     const file = event.target.files ? event.target.files[0] : null;
-     if (file) {
-       const reader = new FileReader();
-       reader.readAsDataURL(file);
-       reader.onloadend = () => {
-         const dataUrl = reader.result as string;
-         setProfilePicture(dataUrl);
-         const formData = new FormData();
-         formData.append('file', file);
-         fetch(`${config.apiUrl}volunteers/profile/`, {
-           method: 'POST',
-           headers: {
-             'Content-Type': 'application/json',
-             'Authorization': `Bearer ${localStorage.getItem('token')}`
-           },
-           body: formData,
-         })
-           .then((response) => {
-             if (response.status === 200) {
-               alert('Profile picture updated successfully');
-             } else {
-               console.log('Error updating profile picture');
-             }
-           })
-           .catch((error) => {
-             console.log(error);
-           });
-       };
-     }
+    const file = event.target.files ? event.target.files[0] : null;
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        const dataUrl = reader.result as string;
+        setProfilePicture(dataUrl);
+        const formData = new FormData();
+        formData.append('file', file);
+        fetch(`${config.apiUrl}volunteers/profile/`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+          },
+          body: formData,
+        })
+          .then((response) => {
+            if (response.status === 200) {
+              alert('Profile picture updated successfully');
+            } else {
+              console.log('Error updating profile picture');
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+    }
   };
-  
-    const updateProfile = () => {
+
+  const updateProfile = () => {
     if (!validateEmail(email)) {
       console.error('Invalid email');
       return;
     }
-    // if (!validatePhone(phone)) {
-    //   console.error('Invalid phone number');
-    //   return;
-    // }
     console.log(first_name, last_name, email, phone, profile_picture);
     let profile: newProfile = {
       first_name: first_name,
@@ -126,37 +117,11 @@ function ProfilePage(props: any) {
     })
   };
 
-  /* Function to add when back is gonna be done */
-
-  /*const deleteAccount = () => {
-     if (window.confirm('Are you sure you want to delete your account?')) {
-      let url = `${config.apiUrl}/volunteers/profile`;
-      fetch(url, {
-        method: 'DELETE',
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
-        },
-      })
-        .then((response) => {
-          if (response.status === 204) {
-            alert('Account deleted successfully');
-            // Redirect to the login page
-            window.location.href = '/';
-          } else {
-            console.log('Error deleting account');
-          }
-        })
-        .catch((error) => 
-          console.log(error);
-        });
-    } */
-
   const [color_blind, setColorBlind] = useState(
     localStorage.getItem("color_blind") === "true"
   );
 
   return (
-    <Container className="profile-container">
       <Row className="profile-row">
         <Col sm={12} md={4} lg={3}>
           <div className="profile-pic">
@@ -224,15 +189,11 @@ function ProfilePage(props: any) {
               <button className={"profile-pic-btn" + ((localStorage.getItem("color_blind") === "true") ? " color-blind-bg" : "")} onClick={updateProfile}>
                 Mettre à jour le profile
               </button>
-              {/* <button className="delete-account-btn" onClick={deleteAccount}>
-                            Supprimer le compte
-                        </button> */}
-                    </div>
-                </div>
-            </Col>
-        </Row>
-    </Container>
+            </div>
+          </div>
+        </Col>
+      </Row>
   );
-};  
+};
 
 export default ProfilePage;
