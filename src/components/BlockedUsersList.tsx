251196d0cdec3462c4174b4f49fcaadf322ea869
Bill ON <bill.on@epitech.eu>
2024-05-07 15:36:28 +0800

    [UPDATE] /volunteer/profile : better component separation

--- src/components/BlockedUsersList.tsx
+++ src/components/BlockedUsersList.tsx
@@ -1,12 +1,44 @@
 import { CardMedia } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import "./BlockedUsersList.scss"
 import config from "../config";
 import { Volunteer } from '../interfaces';
 
-export default function BlockedUsersList(props: { blockedUsers: Volunteer[] }) {
-  const blockedUsers: Volunteer[] = props.blockedUsers;
+
+
+export default function BlockedUsersList() {
+
+  const [blockedUsers, setBlocked] = useState<Volunteer[]>([]);
+  const [isSet, setIsSet] = useState(false);
+
+
+  const getBlocked = () => {
+    fetch(`${config.apiUrl}/friends/blocked`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+    }).then((response) => {
+      if (response.status === 200) {
+        response.json().then((data) => {
+          setBlocked(data);
+        });
+      } else {
+        alert("ERROR FETCHING BLOCKED : " + JSON.stringify(response.body));
+      }
+      setIsSet(true);
+    }).catch((error) => {
+      console.warn(error);
+    });
+  }
+
+  useEffect(() => {
+    if (!isSet) {
+      getBlocked();
+    }
+  }, [isSet]);
 
   const BlockButton = (props: { id: number }) => {
     const id = props.id;
@@ -36,16 +68,24 @@ export default function BlockedUsersList(props: { blockedUsers: Volunteer[] }) {
     );
   }
 
-  const longestName = blockedUsers.reduce((longest, volunteer) => {
-    const fullName = `${volunteer.first_name} ${volunteer.last_name}`;
-    return (fullName.length > longest.length ? fullName : longest) + 1;
-  }, '');
+  let longest = 0;
+
+  for (const u of blockedUsers) {
+    const fullName = `${u.first_name} ${u.last_name}`;
+    const total = (fullName.length > longest) ? fullName.length : longest;
+    longest = (total > 100) ? 100 : total;
+  }
+
+  const getFullName = (u: Volunteer) => {
+    const fullName = `${u.first_name} ${u.last_name}`;
+    return (fullName.length > 100) ? (fullName.slice(0, 100) + "...") : fullName;
+  }
 
   return (
     <div className="blocked-users-container">
       {blockedUsers.map((v: Volunteer) => (
         <div key={v.id} className="volunteer-card-container">
-          <div className="volunteer-card" style={{ width: `${longestName.length * 0.65}em` }}>
+          <div className="volunteer-card" style={{ width: `${longest * 0.65}em` }}>
             <div className="profile-picture-container">
               <img
                 src={v.profile_picture}
@@ -55,7 +95,7 @@ export default function BlockedUsersList(props: { blockedUsers: Volunteer[] }) {
             </div>
             <div className='volunteer-info'>
               <div className="volunteer-name">
-                <span>{v.first_name} {v.last_name}</span>
+                <span>{getFullName(v)}</span>
               </div>
               <BlockButton id={v.id} />
             </div>
