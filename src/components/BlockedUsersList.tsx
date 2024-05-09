6e9be0ff93ac6cf2b97ef4562963ecb57b706004
Louis-Henri MC ALLISTER <louis-henri.mc-allister@epitech.eu>
2024-05-09 10:26:16 -0400

    [FIX] blocked users - make the component reactive

--- src/components/BlockedUsersList.tsx
+++ src/components/BlockedUsersList.tsx
@@ -1,4 +1,4 @@
-import { CardMedia } from '@mui/material';
+import { Alert, Button, CardMedia } from '@mui/material';
 import { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import "./BlockedUsersList.scss"
@@ -9,11 +9,13 @@ import { Volunteer } from '../interfaces';
 
 export default function BlockedUsersList() {
 
+  const [response, setResponse] = useState<{ error: boolean, message: string, id: number }>({ error: false, message: "", id: 0 });
   const [blockedUsers, setBlocked] = useState<Volunteer[]>([]);
   const [isSet, setIsSet] = useState(false);
 
 
   const getBlocked = () => {
+    console.log("Fetching blocked users");
     fetch(`${config.apiUrl}/friends/blocked`, {
       method: 'GET',
       headers: {
@@ -23,7 +25,9 @@ export default function BlockedUsersList() {
     }).then((response) => {
       if (response.status === 200) {
         response.json().then((data) => {
-          setBlocked(data);
+          // filter data by first_name and last_name
+          const filteredData = data.filter((volunteer: Volunteer) => volunteer.first_name && volunteer.last_name);
+          setBlocked(filteredData);
         });
       } else {
         alert("ERROR FETCHING BLOCKED : " + JSON.stringify(response.body));
@@ -40,68 +44,82 @@ export default function BlockedUsersList() {
     }
   }, [isSet]);
 
-  const BlockButton = (props: { id: number }) => {
-    const id = props.id;
-    const [block, setblock] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setResponse({ error: false, message: '', id: 0 });
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [response]);
 
-    const poyo = async () => {
-      const isblock = block ? "block" : "unblock";
-      fetch(`${config.apiUrl}/friends/${isblock}/${id.toString()}`, {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
-      }).then((response) => {
-        if (response.status === 201) {
-          setblock(!block);
+  const BlockUnBlock = async (id: number, isAlreadyBlocked: boolean) => {
+    const isblock = isAlreadyBlocked ? "unblock" : "block";
+    fetch(`${config.apiUrl}/friends/${isblock}/${id.toString()}`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    }).then((response) => {
+      if (response.status === 201) {
+        if (isAlreadyBlocked) {
+          setResponse({ error: false, message: "L'utilisateur a bien été débloqué", id: id });
         } else {
-          console.log(`error trying to ${isblock} id ${id}`);
+          setResponse({ error: false, message: "L'utilisateur a bien été bloqué", id: 0 });
         }
-      })
-    }
-
-    return (
-      <button onClick={poyo} className={`block-button ${block ? "blocked" : "unblocked"}`}>
-        {block ? "Bloquer" : "Débloquer"}
-      </button>
-    );
+        getBlocked();
+      } else {
+        setResponse({ error: true, message: "Une erreur est survenue, veuillez réessayer ultérieurement", id: id });
+      }
+    })
   }
 
-  let longest = 0;
-
-  for (const u of blockedUsers) {
-    const fullName = `${u.first_name} ${u.last_name}`;
-    const total = (fullName.length > longest) ? fullName.length : longest;
-    longest = (total > 100) ? 100 : total;
-  }
-
-  const getFullName = (u: Volunteer) => {
-    const fullName = `${u.first_name} ${u.last_name}`;
+  const getFullName = (volunteer: Volunteer) => {
+    const fullName = `${volunteer.first_name} ${volunteer.last_name}`;
     return (fullName.length > 100) ? (fullName.slice(0, 100) + "...") : fullName;
   }
 
+  const undoUnblock = (id: number) => () => {
+    setResponse({ error: false, message: "", id: 0 });
+    BlockUnBlock(id, false);
+  }
+
   return (
-    <div className="blocked-users-container">
-      {blockedUsers.map((v: Volunteer) => (
-        <div key={v.id} className="volunteer-card-container">
-          <div className="volunteer-card" style={{ width: `${longest * 0.65}em` }}>
-            <div className="profile-picture-container">
-              <img
-                src={v.profile_picture}
-                alt={`${v.first_name} ${v.last_name}`}
-                className="profile-picture"
-              />
-            </div>
-            <div className='volunteer-info'>
-              <div className="volunteer-name">
-                <span>{getFullName(v)}</span>
+    <div>
+      <p className="blocked-user-title">Utilisateurs bloqués</p>
+      <div className="blocked-user-blocked-users-container">
+        {blockedUsers.map((volunteer: Volunteer) => (
+          <div key={volunteer.id} className="blocked-user-volunteer-card-container">
+            <div className="blocked-user-volunteer-card">
+              <div className="blocked-user-profile-picture-container">
+                <img
+                  src={volunteer.profile_picture}
+                  alt={`${volunteer.first_name} ${volunteer.last_name}`}
+                  className="blocked-user-profile-picture"
+                />
               </div>
-              <BlockButton id={v.id} />
+              <div className="blocked-user-volunteer-info">
+                <div className="blocked-user-volunteer-name">
+                  <span>{getFullName(volunteer)}</span>
+                </div>
+                <Button onClick={() => BlockUnBlock(volunteer.id, true)} variant="contained" color="error">
+                  Débloquer
+                </Button>
+              </div>
             </div>
           </div>
+        ))}
+      </div>
+      {response.message &&
+        <div className="blocked-user-alert">
+          <Alert severity={response.error ? "error" : "success"}
+            {...(!response.error && response.id !== 0) &&
+            { action: <Button onClick={undoUnblock(response.id)}>Annuler</Button> }
+            }
+          >
+            {response.message}
+          </Alert>
         </div>
-      ))}
+      }
     </div>
   );
 }
