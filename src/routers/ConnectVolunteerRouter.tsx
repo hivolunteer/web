f8cc3e04576d8dcba8528dfb6ba235206bbd625f
Pyer Tran <pierre.tran@epitech.eu>
2024-05-10 17:08:11 +0900

    [FIX] PR requirements, + fix timeout system on new alerts

--- src/routers/ConnectVolunteerRouter.tsx
+++ src/routers/ConnectVolunteerRouter.tsx
@@ -11,7 +11,7 @@ import ChangePassword from "../pages/Volunteer/Settings/ModifyPassword";
 import PublicProfile from "../pages/Volunteer/PublicProfile/PublicProfile";
 
 import MissionCreation from "../pages/Volunteer/Missions/MissionCreation";
-import MissionModification from "../pages/Volunteer/Missions/MissionModifiy";
+import EditMission from "../pages/Volunteer/Missions/MissionModify";
 
 import AssociationReferent from "../pages/Volunteer/Settings/Referents/AssociationReferent";
 import AssociationProfile from "../pages/Volunteer/Association/Profile/AssociationProfile";
@@ -30,7 +30,7 @@ function ConnectVolunteerRouter() {
             <Route path="/association/:associationID" element={<AssociationProfile />} />
             <Route path="/mission/:missionID" element={<MissionDetails />} />
             <Route path="/mission/create" element={<MissionCreation/>} />
-      <Route path="/mission/edit/:missionID" element={<MissionModification/>} />
+      <Route path="/mission/edit/:missionID" element={<EditMission/>} />
             <Route path="/volunteer/:volunteerId" element={<PublicProfile/>} />
             <Route path={"/settings"} element={<Settings />} />
             <Route path="/settings/modify_password" element={<ChangePassword />} />
