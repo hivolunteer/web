27a3f79dbac17db6759feb4e74173b69952da774
Petro Floresku <petro.floresku@epitech.eu>
2024-05-10 15:48:14 +0200

    [WEB-4.2]: resolve merge conflicts

--- src/routers/ConnectVolunteerRouter.tsx
+++ src/routers/ConnectVolunteerRouter.tsx
@@ -26,16 +26,17 @@ function ConnectVolunteerRouter() {
             <Route path="/follows" element={<FollowAssociation />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/profile/blocked" element={<BlockedUsersList />} />
+            <Route path="/profile/blocked" element={<BlockedUsersList />} />
             <Route path="/accueil" element={<Search />} />
             <Route path="/association/:associationID" element={<AssociationProfile />} />
             <Route path="/mission/:missionID" element={<MissionDetails />} />
             <Route path="/mission/create" element={<MissionCreation/>} />
       <Route path="/:missionID/edit" element={<EditMission/>} />
-            <Route path="/volunteer/:volunteerId" element={<PublicProfile/>} />
-            <Route path={"/settings"} element={<Settings />} />
+                  <Route path="/volunteer/:volunteerId" element={<PublicProfile/>} />
+                  <Route path={"/settings"} element={<Settings />} />
             <Route path="/settings/modify_password" element={<ChangePassword />} />
             <Route path="/settings/referents" element={<AssociationReferent />} />
-            <Route path="/manage/:id" element={<ManageMission />} />
+                  <Route path="/manage/:id" element={<ManageMission />} />
             <Route path="/settings/profile_information" element={<ProfileInformationModal />} />
             <Route path="*" element={<h1> Error 404 Volunteer </h1>} />
         </Routes>
