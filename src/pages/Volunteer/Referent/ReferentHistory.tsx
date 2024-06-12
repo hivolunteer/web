fbbc9568b5a99a06987a19f8a6c162c7833513ea
Louis-Henri MC ALLISTER <louis-henri.mc-allister@epitech.eu>
2024-06-12 09:12:49 +0200

    HIVE-XXX [ADD] referent volunteer mission page

---
+++ src/pages/Volunteer/Referent/ReferentHistory.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReferentHistoryList from './ReferentHistoryList';
+import TabPanel from "../Search/Panels/TabPanel";
+import { Tab, Tabs } from '@mui/material';
+import './ReferentHistory.scss'
+
+function ReferentHistory() {
+    const [value, setValue] = React.useState(1);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div style={{ marginTop: '20px', marginLeft: '10px' }}>
+            <p className="referent-history-title"> Historique des missions </p>
+            <p className="referent-history-description">Retrouvez ici l'historique des missions pour lesquelles vous êtes référent.</p>
+            <div className="referent-history-container">
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" variant="fullWidth">
+                    <Tab label="En cours" value={1} />
+                    <Tab label="Passées" value={2} />
+                </Tabs>
+                <TabPanel value={value} index={1}>
+                    <ReferentHistoryList is_active={true} />
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    <ReferentHistoryList is_active={false} />
+                </TabPanel>
+            </div>
+        </div>
+    )
+}
+
+export default ReferentHistory;
