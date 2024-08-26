2daaefc9a9b85b97bfb27162f5038437e97750ad
Petro Floresku <petro.floresku@epitech.eu>
2024-08-26 19:34:31 +0200

    ADD profile page without recuperation of informations

--- src/routers/ConnectCompanyRouter.tsx
+++ src/routers/ConnectCompanyRouter.tsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Company/Home/Home';
 import Settings from '../pages/Company/Settings/Settings';
+import ProfileInformationModal from '../pages/Company/Settings/ProfileInformation';
 
 function ConnectCompanyRouter() {
     return(
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/settings/profile_information" element={<ProfileInformationModal />} />
             <Route path="*" element={<h1> Error 404 Asso </h1>} />
         </Routes>
     )
