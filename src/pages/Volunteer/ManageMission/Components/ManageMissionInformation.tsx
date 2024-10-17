550f4911972466cc243040d6bdd4d006fb435c49
Bill ON <bill.on@epitech.eu>
2024-10-18 00:33:16 +0200

    [FIX] display theme name on mission manager

--- src/pages/Volunteer/ManageMission/Components/ManageMissionInformation.tsx
+++ src/pages/Volunteer/ManageMission/Components/ManageMissionInformation.tsx
@@ -4,6 +4,7 @@ import Grid from "@mui/system/Unstable_Grid";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import config from "../../../../config";
 import './ManageMissionInformation.scss';
+import getTheme from "../../../../functions/getTheme";
 
 interface Mission {
   id: number,
@@ -34,17 +35,17 @@ function TextDisplayer(props: any) {
   const element = props.element;
 
   if (element === null || element === undefined) {
-      return (
-        <div className="manage-mission-information-container">
-          <p className="manage-mission-information-container-text">Non défini</p>
-        </div>
-      );
+    return (
+      <div className="manage-mission-information-container">
+        <p className="manage-mission-information-container-text">Non défini</p>
+      </div>
+    );
   } else {
-      return (
-        <div className="manage-mission-information-container">
-          { element }
-        </div>
-      );
+    return (
+      <div className="manage-mission-information-container">
+        {element}
+      </div>
+    );
   }
 }
 
@@ -61,6 +62,7 @@ function ManageMissionInformation(props: ManageMissionInformationProps) {
   const [mission, setMission] = useState<Mission>();
   const [location, setLocation] = useState<Location>();
   const [missionPicture, setMissionPicture] = useState<string>("");
+  const [theme, setTheme] = useState<string>('');
 
   const mission_id = props.mission_id;
   const SetMissionStatus = props.setMissionStatus;
@@ -69,7 +71,7 @@ function ManageMissionInformation(props: ManageMissionInformationProps) {
 
   function formatDate(date: string) {
     if (date === '')
-        return ''
+      return ''
     let day = date.split('T')[0].split('-')[2]
     let month = date.split('T')[0].split('-')[1]
     let year = date.split('T')[0].split('-')[0]
@@ -79,110 +81,117 @@ function ManageMissionInformation(props: ManageMissionInformationProps) {
   }
 
   useEffect(() => {
+    let missionData: Mission | undefined = mission;
     fetch(`${config.apiUrl}/missions/${isAssociation ? 'association' : 'close'}/${mission_id}`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
     }).then((response) => {
-        if (response.status === 200) {
-            response.json().then((data) => {
-                const mission = (isAssociation ? data.association_mission : data.close_mission)
-                setMission(mission);
-                SetMissionStatus(mission.status);
-								SetMissionEndDate(mission.end_date);
-                setMissionPicture(mission.picture);
-                fetch(`${config.apiUrl}/locations/${mission.location}`, {
-                    method: 'GET',
-                    headers: {
-                        'content-type': 'application/json',
-                        'Authorization': `Bearer ${localStorage.getItem('token')}`
-                    }
-                }).then((response) => {
-                    if (response.status === 200) {
-                        response.json().then((data) => {
-                            setLocation(data);
-                        })
-                    }
+      if (response.status === 200) {
+        response.json().then((data) => {
+          const mission = (isAssociation ? data.association_mission : data.close_mission)
+          missionData = mission;
+          setMission(mission);
+
+          getTheme(localStorage.getItem('token') as string, mission.theme_id).then((theme) => {
+            setTheme(theme);
+          });
+    
+          SetMissionStatus(mission.status);
+          SetMissionEndDate(mission.end_date);
+          setMissionPicture(mission.picture);
+          fetch(`${config.apiUrl}/locations/${mission.location}`, {
+            method: 'GET',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+          }).then((response) => {
+            if (response.status === 200) {
+              response.json().then((data) => {
+                setLocation(data);
+              })
+            }
+          })
+          console.log("MISSION PICTURE", missionPicture);
+          if (mission.picture && mission.picture.startsWith('/uploads')) {
+            fetch(`${config.apiUrl}/uploads/${isAssociation ? 'association' : 'volunteer'}/mission/${mission_id}`, {
+              method: 'GET',
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+              },
+            }).then((response) => {
+              console.log(response);
+              response.blob()
+                .then((blob) => {
+                  const objectUrl = URL.createObjectURL(blob);
+                  setMissionPicture(objectUrl);
                 })
-                console.log("MISSION PICTURE", missionPicture);
-                if (mission.picture && mission.picture.startsWith('/uploads')) {
-                  fetch(`${config.apiUrl}/uploads/${isAssociation ? 'association' : 'volunteer'}/mission/${mission_id}`, {
-                      method: 'GET',
-                      headers: {
-                          Authorization: `Bearer ${localStorage.getItem('token')}`
-                      },
-                  }).then((response) => {
-                      console.log(response);
-                      response.blob()
-                        .then((blob) => {
-                          const objectUrl = URL.createObjectURL(blob);
-                          setMissionPicture(objectUrl);
-                        })
-                    .catch((error) => {
-                      console.error(error);
-                    });
-                  });
-                }
-            }) 
-        } else {
-            window.location.href = "/";
-        }
-    })
+                .catch((error) => {
+                  console.error(error);
+                });
+            });
+          }
+        })
+      } else {
+        window.location.href = "/";
+      }
+    });
   }, []);
 
-    return (
-      <div>
-        <div className="manage-mission-information-header-container" style={{backgroundImage: `url(${missionPicture})`}}>
-          {/* <div className="association-logo">
+  return (
+    <div>
+      <div className="manage-mission-information-header-container" style={{ backgroundImage: `url(${missionPicture})` }}>
+        {/* <div className="association-logo">
             <img src="https://th.bing.com/th/id/R.a159530285fe4c5b20f40dc89741304e?rik=3L6mcWO3XWPxxA&pid=ImgRaw&r=0.png" alt="logo" className='association-logo-mission'/>
           </div> */}
-          <div className="manage-mission-information-header-container-title">
-            <h1> { mission?.title } </h1>
-          </div>
+        <div className="manage-mission-information-header-container-title">
+          <h1> {mission?.title} </h1>
         </div>
-        <Accordion className="volunteer-manage-mission-accordion" defaultExpanded={true}>
-          <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-          >
-              Informations de la mission
-          </AccordionSummary>
-          <AccordionDetails>
-              <Grid container spacing={3}>
-                  <Grid xs={6}>
-                      <h4> Date de la mission </h4>
-                      <div className="manage-mission-information-container">
-                      Du {formatDate(mission?.start_date?.toString() ?? '')} au {formatDate(mission?.end_date?.toString() ?? '')}
-                      </div>
-                      <h4> Lieu </h4>
-                      <div className="manage-mission-information-container">
-                      {location?.street_number} {location?.street_number_suffix} {location?.street_type} {location?.street_name}, {location?.postal_code} {location?.city}
-                      </div>
-                      <h4> Nombre de volontaires </h4>
-                      <TextDisplayer element={mission?.max_volunteers} />
-                      <h4> Thème </h4>
-                      <TextDisplayer element={mission?.theme_id} />
-                      <h4> Status </h4>
-                      <p className={props.MissionStatus === 1 ? "manage-mission-information-status-published" : props.MissionStatus === 2 ? "manage-mission-information-status-canceled" : props.MissionStatus === 3 ? "manage-mission-information-status-passed" : "manage-mission-information-status-draft"}>
-                        { props.MissionStatus === 1 ? "En ligne" : props.MissionStatus === 2 ? "Annulée" : props.MissionStatus === 3 ? "Passée" : "Brouillon" }
-                      </p>
+      </div>
+      <Accordion className="volunteer-manage-mission-accordion" defaultExpanded={true}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          Informations de la mission
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container spacing={3}>
+            <Grid xs={6}>
+              <h4> Date de la mission </h4>
+              <div className="manage-mission-information-container">
+                Du {formatDate(mission?.start_date?.toString() ?? '')} au {formatDate(mission?.end_date?.toString() ?? '')}
+              </div>
+              <h4> Lieu </h4>
+              <div className="manage-mission-information-container">
+                {location?.street_number} {location?.street_number_suffix} {location?.street_type} {location?.street_name}, {location?.postal_code} {location?.city}
+              </div>
+              <h4> Nombre de volontaires </h4>
+              <TextDisplayer element={mission?.max_volunteers} />
+              <h4> Thème </h4>
+              <TextDisplayer element={theme} />
+              <h4> Status </h4>
+              <p className={props.MissionStatus === 1 ? "manage-mission-information-status-published" : props.MissionStatus === 2 ? "manage-mission-information-status-canceled" : props.MissionStatus === 3 ? "manage-mission-information-status-passed" : "manage-mission-information-status-draft"}>
+                {props.MissionStatus === 1 ? "En ligne" : props.MissionStatus === 2 ? "Annulée" : props.MissionStatus === 3 ? "Passée" : "Brouillon"}
+              </p>
 
-                  </Grid>
-                  <Grid xs={6}>
-                      <h4> Description de la mission </h4>
-                      <TextDisplayer element={mission?.description} />
-                      <h4> Informations pratiques </h4>
-                      <TextDisplayer element={mission?.practical_information} />
-                  </Grid>
-              </Grid>
-          </AccordionDetails>
-          <AccordionActions>
-              <Button size="small" color="warning" onClick={() => window.location.href = `/${mission?.id}/edit`}>Modifier</Button>
-              <Button size="small" color="info" onClick={() => window.location.href = `/mission/${mission?.id}`}>Visualiser</Button>
-          </AccordionActions>
+            </Grid>
+            <Grid xs={6}>
+              <h4> Description de la mission </h4>
+              <TextDisplayer element={mission?.description} />
+              <h4> Informations pratiques </h4>
+              <TextDisplayer element={mission?.practical_information} />
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+        <AccordionActions>
+          <Button size="small" color="warning" onClick={() => window.location.href = `/${mission?.id}/edit`}>Modifier</Button>
+          <Button size="small" color="info" onClick={() => window.location.href = `/mission/${mission?.id}`}>Visualiser</Button>
+        </AccordionActions>
       </Accordion>
     </div>
   );
