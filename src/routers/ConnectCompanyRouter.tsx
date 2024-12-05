dbbf1170e6219be28c9d746ab9495f2259701919
Louis-Henri MC ALLISTER <hivolunteer@outlook.fr>
2024-12-05 16:50:08 +0100

    [ADD] Uniformisation des routes ranking pour les employés et pour l'entreprise + rajout d'une pagination et de statistiques

--- src/routers/ConnectCompanyRouter.tsx
+++ src/routers/ConnectCompanyRouter.tsx
@@ -9,6 +9,7 @@ import ModifyPassword from '../pages/Company/Settings/ModifyPassword';
 import Missions from '../pages/Company/Missions/Missions';
 import FAQ from '../pages/Company/FAQ/Faq';
 import ContactUs from '../pages/Contact Us/Contact';
+import EmployeeRanking from '../pages/Company/Employee/EmployeeRanking';
 
 function ConnectCompanyRouter() {
     return(
@@ -23,6 +24,7 @@ function ConnectCompanyRouter() {
             <Route path={"/missions"} element={<Missions />} />
             <Route path="/faq" element={< FAQ />} />
             <Route path="/contact" element={< ContactUs />} />
+            <Route path="/rank" element={<EmployeeRanking />} />
             <Route path="*" element={<h1> Error 404 Company </h1>} />
         </Routes>
     )
